15d4fe0a72e7ee958b09ca141e2d5e5ebf053961
Lucia Balsa <lbalsamartinez@danielcastelao.org>
2022-12-07 14:29:07 +0100

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
                 //el lider (absoluto): crear projecto, subir repo a github, añade al colaborador, el lider (todopoderoso) crea varios commits en la rama main
                 //el colaborador (la plebe): clona el proyecto/ repo, crea la rama colab, hace commits (en la main), sube la rama al github
                 //lider (magistral): hace un pullrequest en la rama colab, un merge en /conflict, subir resultado
+
+                //commit 1 Lu rama colab
         }
 }
