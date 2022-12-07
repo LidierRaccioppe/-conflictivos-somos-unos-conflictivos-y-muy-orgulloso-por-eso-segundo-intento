2bfffdeeefe7ff689533f240054c5ecbfd4e6d4e
Lidier López <llopezraccioppe@danielcastelao.org>
2022-12-07 14:25:22 +0100

    ahhhhh, son una "a" y cinco "h"

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+        public static void main(String[] args) {
+                //el lider (absoluto): crear projecto, subir repo a github, añade al colaborador, el lider (todopoderoso) crea varios commits en la rama main
+                //el colaborador (la plebe): clona el proyecto/ repo, crea la rama colab, hace commits (en la main), sube la rama al github
+                //lider (magistral): hace un pullrequest en la rama colab, un merge en /conflict, subir resultado
+        }
+}
